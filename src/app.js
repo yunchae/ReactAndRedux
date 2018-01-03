c7964510de72ad1a604efe3bdf2f273ccfd0acf3
yunchae.na <ck2204@naver.com>
2018-01-03 17:21:50 +0900

    2.Setting up the Redux Store

--- src/app.js
+++ src/app.js
@@ -3,16 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import reducer from './reducer'
+
 require('./index.html');
+
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
 
 // Create app
 const container = document.querySelector('#app-container');
 
+console.log('store', store);
+
 // Render app
 ReactDOM.render(
-    <AppContainer>
+    <Provider store={store}>
         <App />
-    </AppContainer>
+    </Provider>
     , container
 );
 
